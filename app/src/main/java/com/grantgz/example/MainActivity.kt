a27a4d3953d545f59b26fd358fb4a3143d8ebfc2
adazhdw <dgz102486@163.com>
2019-07-04 14:27:43 +0800

    添加测试页面

--- app/src/main/java/com/grantgz/example/MainActivity.kt
+++ app/src/main/java/com/grantgz/example/MainActivity.kt
@@ -2,11 +2,14 @@ package com.grantgz.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val videoUrl = "https://file.yizhujiao.com/o_1cm4d1a1t1uicke618cb4k39013.mp4"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mExoPlayerView.setDataSource(videoUrl)
     }
 }
